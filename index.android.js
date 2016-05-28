79e139026ec9c2a2b17f239c03d9171f5c40ae93
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2016-05-28 20:20:47 +0100

    unused styles removed

--- index.android.js
+++ index.android.js
@@ -178,21 +178,6 @@ const styles = StyleSheet.create({
 		alignItems: 'center',
 		backgroundColor: '#F5FCFF'
 	},
-	rightContainer: {
-		flex: 1
-	},
-	thumbnail: {
-		width: 53,
-		height: 81
-	},
-	title: {
-		fontSize: 20,
-		marginBottom: 8,
-		textAlign: 'center'
-	},
-	year: {
-		textAlign: 'center'
-	},
 	center: {
 		alignItems: 'center'
 	},
